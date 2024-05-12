ddeafd193910f381feddc4deca05a8df614993f3
PolatMurat41 <sea.ase112@gmail.com>
2024-05-13 01:09:07 +0300

    Container component for FoodIItem(children)

--- foodapp/src/App.jsx
+++ foodapp/src/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import Search from "./components/Search";
 import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
-import './App.css'
+import "./App.css";
+import Container from "./components/Container";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData}/>
+      <Container >
+        <FoodList foodData={foodData} />
+      </Container>
     </div>
   );
 }
